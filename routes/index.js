b81505268bb3d2168af69cd4dd30fa5adbc09def
cristianocorrea3 <94192765+cristianocorreamoraes@users.noreply.github.com>
2022-10-25 09:59:52 -0300

    Teste de CI/CD.
    Teste de CI/CD.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'TESTE DEPLOY AZURE - SENAI' });
+  res.render('index', { title: '---- #### TESTE DEPLOY AZURE - SENAI #### ----' });
 });
 
 module.exports = router;
